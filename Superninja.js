9df3ed3415b55c4f27a9a6e61fb7fdb797627c6c
VishnuBhaarath <vishnubhaarath30@gmail.com>
2019-05-20 17:16:25 +0530

    addingwebpage

---
+++ Superninja.js
@@ -0,0 +1,7 @@
+var button=document.getElementById('Submit')
+var weaponimages=document.getElementById('weaponimages')
+weaponimages.style.display="none"
+
+button.addEventListener("click", function(){
+   weaponimages.style.display="block";
+  })           
